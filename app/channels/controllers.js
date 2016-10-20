cff9529fd63c24328081bde9a7160aae72d3770c
thivolle cedric <cedric@thivolle-cazat.fr>
2016-10-20 22:06:18 +0200

    :wip channels testing

--- app/channels/controllers.js
+++ app/channels/controllers.js
@@ -1,5 +1,5 @@
 angular.module('voxity.channels').controller('makeCallCtrl', [
-    '$scope', 'vxtCoreApi',
+    '$scope', 'vxtApiChannels',
     function ($scope, apiChannels) {
         $scope.callProcessing = false;
 
@@ -15,5 +15,56 @@ angular.module('voxity.channels').controller('makeCallCtrl', [
         $scope.checkNumber = function(){
             this.phoneNumber = $scope.phoneNumber.replace(/[^\+\d\(\)]/g,'');
         }
+    }
+])
+
+angular.module('voxity.channels').controller('channelsCtrl', [
+    '$scope', 'vxtCoreApi',
+    function ($scope, apiChannels) {
+        $scope.errors = {err: false,mess:''};
+        $scope.channels = {};
+        
+
+        $scope.init = function(){
+            $scope.errors = {err: false,mess:''};
+            $scope.loading = true;
+            if (!api.token){
+                return null;
+            } else {
+                apiChannels.get(function(err, channels){
+                    if (err){
+                        $scope.errors.err = true;
+                        $scope.errors.mess = "Une erreur est survenu lors du chargement des appels en cours. err"+err.status;
+                        $scope.channels = {};
+                    } else {
+                        angular.forEach(channels, function(c){
+                            if(c.caller_num != c.exten){
+                                // is incoming call
+                                if (!$scope.channels[c.exten]) {
+                                    $scope.channels[c.exten] = [];
+                                }
+                                $scope.channels[c.exten].push({
+                                    name: c.callee_num || c.caller_name
+                                    num: c.caller_num
+                                })  
+                                
+                            } else {
+                                // outroutin call
+                                if (!$scope.channels[c.exten]) {
+                                    $scope.channels[c.exten] = [];
+                                }
+                                $scope.channels[c.exten].push({
+                                    name:c.caller_name,
+                                    num:c.caller_num
+
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        }
+        $scope.$on('api:TOKEN_SET', $scope.init);
+
     }
 ])
